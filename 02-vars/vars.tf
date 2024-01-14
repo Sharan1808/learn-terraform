7e9a5927487a70c528d60560815101badba39543
Sharan <sharanpyata1808@gmail.com>
2024-01-13 19:10:08 -0600

    Terraform sample changes

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -13,3 +13,15 @@ variable "fruits" {
 output "fruits_first_fruit" {
   value = var.fruits[0]
 }
+
+variable "fruits_with_quantity" {
+  default = {
+    apple = 100
+    banana = 200
+    orange = 300
+  }
+}
+
+output "fruits_apple_quantity" {
+  value = var.fruits_with_quantity["apple"]
+}
