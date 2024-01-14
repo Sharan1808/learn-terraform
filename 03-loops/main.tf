b00ff1257d96efdfcbc7f8c56780bc856efdcd09
Sharan <sharanpyata1808@gmail.com>
2024-01-13 22:33:18 -0600

    Terraform loop changes

--- 03-loops/main.tf
+++ 03-loops/main.tf
@@ -12,3 +12,20 @@ variable "instances" {
 resource "null_resource" "sample" {
 count = length(var.instances)
 }
+
+variable "instances1" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    backend = {
+      name = "backend"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+  }
+}
